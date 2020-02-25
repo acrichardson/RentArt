c73c9a97d21c848a8c39ccf922856d25d3ca6bbd
sara faouzi <sara.faouzi97@gmail.com>
2020-02-25 11:45:36 +0100

    Added the reviews Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,15 @@
 class ReviewsController < ApplicationController
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
 end
