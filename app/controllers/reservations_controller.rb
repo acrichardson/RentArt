d4637cee22042d3726501ff1da0a4e3427cf3f1d
Ashanti Richardson <ashanti.richardson@gmail.com>
2020-02-25 12:31:47 +0100

    reservations_controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,35 @@
 class ReservationsController < ApplicationController
+    def index
+    @reservations = Reservation.all
+  end
 
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+    #   redirect_to @reservations_path
+    # else
+    #   render :new
+  end
+
+  def destroy
+    @reservation = @reservation.find(params[:id])
+    @reservation.destroy
+  end
+
+  def show
+    @reservation = @reservation.find(params[:id])
+  end
+
+  private
+  # def get_reservation
+  #   @reservation = @reservation.find(params[:id])
+  # end
+
+  def reservation_params
+    params.require(:reservations).permit(:id, :date, :product, :user, :review)
+  end
 end
