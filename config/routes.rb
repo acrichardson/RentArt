c7d12854ccb82e1fbfb153b005952a5107716eef
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-02-25 12:17:38 +0100

    needed to add a block for reservations routes in order to get correct route for review

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products do
-    resources :reservations, only: [:new, :create]
-    resources :reviews, only: [:new, :create]
+    resources :reservations, only: [:new, :create] do
+      resources :reviews, only: [:new, :create]
+    end
   end
    resources :reviews, only: [:destroy]
   resources :reservations, only: [:destroy, :show]
