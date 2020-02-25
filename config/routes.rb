2e681a664343053580017454eb631576ac8f17a5
Ashanti Richardson <ashanti.richardson@gmail.com>
2020-02-25 14:44:05 +0100

    Added rough form and navbar views and scss

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products do
-    resources :reservations, only: [:new, :create]
+    resources :reservations, only: [:new, :create, :index]
     resources :reviews, only: [:new, :create]
   end
    resources :reviews, only: [:destroy]
