7812d701be5a5a579a62ba5b1e7e5b12b3487ec1
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-02-26 12:11:03 +0100

    reservation form is added and now has a start and end date and links to reservation new once created

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,12 +2,13 @@ class ReservationsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @reservations = Reservation.all
+     @reservations = policy_scope(Reservation).order(created_at: :desc)
   end
 
   def new
     @product = Product.find(params[:product_id])
     @reservation = Reservation.new
+    authorize @reservation
   end
 
   def create
@@ -16,6 +17,7 @@ class ReservationsController < ApplicationController
     @user = current_user
     @reservation.product = @product
     @reservation.user = @user
+    authorize @reservation
     if @reservation.save
     #   redirect_to @reservations_path
     # else
@@ -26,11 +28,13 @@ class ReservationsController < ApplicationController
   def destroy
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
+    authorize @reservation
   end
 
   def show
     @reservation = Reservation.find(params[:id])
     @review = Review.new
+    authorize @reservation
   end
 
 private
@@ -39,6 +43,6 @@ private
   # end
 
   def reservation_params
-    params.require(:reservations).permit(:id, :date, :product_id, :user_id, :review_id)
+    params.require(:reservation).permit(:id, :date, :product_id, :user_id, :review_id, :reservations)
   end
 end
